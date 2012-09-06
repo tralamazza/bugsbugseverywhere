351d966d9fd61e45365cdeb22aae965cbe6e26a2
Daniel Tralamazza <tralamazza@gmail.com>
2012-09-07 01:29:13 +0200

    use sessions

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var express = require('express');
 
 var app = express();
 
-app.locals.signed = false;
-
 app.configure(function() {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'run to the hills, run for your lives', cookie: { maxAge: 60000 } }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { signed: req.session.signed });
 });
 
 app.get('/login', function(req, res) {
@@ -20,12 +20,12 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-  app.locals.signed = true;
+  req.session.signed = true;
   res.redirect('/');
 });
 
 app.get('/logout', function(req, res) {
-  app.locals.signed = false;
+  req.session.signed = false;
   res.redirect('/login');
 });
 
